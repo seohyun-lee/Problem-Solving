1666b9d6a34373047f6e60a9817ccc27514b5ff2
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-05-27 19:34:27 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -1,18 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main()
 {
 	int a[3], b[3], i;
-	char num;
 	for (i = 2; i >= 0; i--)
 		scanf("%1d", &a[i]);
-	for (i = 2; i >= 0; i--){
+	for (i = 2; i >= 0; i--)
 		scanf("%1d", &b[i]);
-		if (100 * a[0] + 10 * a[1] + a[2] > 100 * b[0] + 10 * b[1] + b[2])
-			num = 'a';
-		else
-			num = 'b';
-	}
-	if(num == 'a')
+	if (100 * a[0] + 10 * a[1] + a[2] > 100 * b[0] + 10 * b[1] + b[2])
 		printf("%d%d%d", a[0], a[1], a[2]);
 	else
 		printf("%d%d%d", b[0], b[1], b[2]);
