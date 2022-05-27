01116e1223ea100c82171c0fd9fad4e75e6acec8
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-05-27 19:16:02 +0900

    Update 단어의 개수.c

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main()
 {
 	char str[1000000] = { 0 };
@@ -18,6 +18,30 @@ int main()
 		if (i == 0)
 			cnt++;
 		if (str[i] == ' ')
+			cnt++;
+		i++;
+	}
+	printf("%d", cnt);
+	return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+	char str[1000000] = { 0 };
+	int cnt = 0;
+	int i = 0;
+	while (1) {
+		scanf("%c", &str[i]);
+		if (i == 0 && str[i] == ' ')
+			continue;
+		if (str[i] == '\n')
+		{
+			if(str[i - 1] == ' ')
+                cnt--;
+			break;
+		}
+		if (i == 0 || str[i] == ' ')
 			cnt++;
 		i++;
 	}
