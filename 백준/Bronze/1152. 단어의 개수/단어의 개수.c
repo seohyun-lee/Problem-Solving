b796e324ef02afa4179c7daf9048977dd9ae1bac
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-05-27 19:22:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,49 +1,22 @@
-/*#include<stdio.h>
 int main()
 {
-	char str[1000000] = { 0 };
+	char pw='s', w;
 	int cnt = 0;
 	int i = 0;
 	while (1) {
-		scanf("%c", &str[i]);
-		if (i == 0 && str[0] == ' ')
+		scanf("%c", &w);
+		if (i == 0 && w == ' ')
 			continue;
-		if (str[i] == '\n' && str[i - 1] == ' ')
+		if (w == '\n')
 		{
-			cnt--;
+			if (pw == ' ')
+				cnt--;
 			break;
 		}
-		if (str[i] == '\n')
-			break;
-		if (i == 0)
-			cnt++;
-		if (str[i] == ' ')
+		if (i == 0 || w == ' ')
 			cnt++;
 		i++;
-	}
-	printf("%d", cnt);
-	return 0;
-}*/
-
-#include<stdio.h>
-int main()
-{
-	char str[1000000] = { 0 };
-	int cnt = 0;
-	int i = 0;
-	while (1) {
-		scanf("%c", &str[i]);
-		if (i == 0 && str[i] == ' ')
-			continue;
-		if (str[i] == '\n')
-		{
-			if(str[i - 1] == ' ')
-                cnt--;
-			break;
-		}
-		if (i == 0 || str[i] == ' ')
-			cnt++;
-		i++;
+		pw = w;
 	}
 	printf("%d", cnt);
 	return 0;
