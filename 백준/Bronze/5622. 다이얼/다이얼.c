1c166dfbd6a3a9f49f0d24d38a84af98ef746912
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-05-27 19:51:11 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.c
+++ 백준/Bronze/5622. 다이얼/다이얼.c
@@ -1,5 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
 int main() {
 	int time = 0, i = 0;
 	while (1)
@@ -8,8 +6,7 @@ int main() {
 		scanf("%c", &c);
 		if (c == '\n')
 			break;
-		int a = (c - 65) / 3;
-		time = time + a + 3;
+		time = time + (c - 65) / 3 + 3;
 		if (c == 'S' || c == 'V' || c == 'Y' || c == 'Z')
 			time--;
 	}
