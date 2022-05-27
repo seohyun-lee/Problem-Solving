dddfa36aadb2bb35fcf44fc592762876d4a616db
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-05-27 19:08:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,14 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main()
 {
-	char str[1000000] = { 0, };
+	char str[1000000] = { 0 };
 	int cnt = 0;
 	int i = 0;
 	while (1) {
-		char c;
-		scanf("%c", &c);
-		str[i]=c;
+		scanf("%c", &str[i]);
 		if (i == 0 && str[0] == ' ')
 			continue;
 		if (str[i] == '\n' && str[i - 1] == ' ')
@@ -19,7 +16,7 @@ int main()
 		if (str[i] == '\n')
 			break;
 		if (i == 0)
-			cnt = 1;
+			cnt++;
 		if (str[i] == ' ')
 			cnt++;
 		i++;
