b72a38bf07e2ad76cb5ed43b620b02a061642e0f
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-09-02 03:00:42 +0900

    [Silver IV] Title: 알바생 강호, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int N = 0;
+	long long int MAX=0;
+	cin >> N;
+	int* arr = new int[N];
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++) {
+		arr[i] = arr[i] - (N-1-i);
+		if (arr[i] > 0)
+			MAX += arr[i];
+	}
+	cout << MAX;
+	delete[] arr;
+	return 0;
+}
