9bd86df035a27eca665bbe5b37a4af2448c94a37
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-09-02 01:35:16 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 수 정렬하기 - 2750 
+
+[문제 링크](https://www.acmicpc.net/problem/2750) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 40 ms
+
+### 분류
+
+정렬(sorting), 구현(implementation)
+
---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int N;
+	int arr[1000] = {};
+	cin>>N;
+	for (int i = 0; i < N; i++){
+		cin >> arr[i];
+	}
+	sort(arr, arr + N);
+	for (int i = 0; i < N; i++){
+		cout << arr[i] << endl;
+	}
+	return 0;
+}
