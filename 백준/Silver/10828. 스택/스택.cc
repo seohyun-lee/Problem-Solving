dc2301ba4d143dcda008c251380970e7b70a512f
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-09-09 17:32:51 +0900

    [Silver IV] Title: 스택, Time: 428 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,71 +1,50 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
 const int SIZE = 1e4;
 vector<int> stack_vec(SIZE); //와이?
 int top_pointer = -1;
 
-bool full() {
-	return top_pointer == (SIZE - 1);
-}
-
-void push(int x) {
-	stack_vec[++top_pointer] = x;
-}
-
-void pop() {
-	top_pointer--;
-}
-
-int size() {
-	return top_pointer + 1;
-}
-
-bool empty() {
-	return top_pointer == -1;
-}
-
-int top() {
-	return stack_vec[top_pointer];
-}
-
 int main() {
 	int n, x;
 	string cmd;
+	stack<int> st;
+
 	cin >> n;
 	while (n--) {
 		cin >> cmd;
 		if (cmd == "push") {
 			cin >> x;
-			if(!full())
-				push(x);
+			st.push(x);
 			continue;
 		}
 		if (cmd == "pop") {
-			if(empty()){
+			if (st.empty()) {
 				cout << "-1\n";
-			} else {
-				cout << top() << '\n';
-				pop();
+			}
+			else {
+				cout << st.top() << '\n';
+				st.pop();
 			}
 			continue;
 		}
 		if (cmd == "size") {
-			cout << size() << '\n';
+			cout << st.size() << '\n';
 			continue;
 		}
 		if (cmd == "empty") {
-			cout << empty() << '\n';
+			cout << st.empty() << '\n';
 			continue;
 		}
 		if (cmd == "top") {
-			if (empty()) {
+			if (st.empty()) {
 				cout << "-1\n";
-			}else {
-				cout << top() << '\n';
+			}
+			else {
+				cout << st.top() << '\n';
 			}
 		}
 	}
-	return 0;
 }
