4175dfd07561d3fe902bac7868e7584f7139746a
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2023-03-27 11:29:09 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int score=input.nextInt();
+		if(score>=90) System.out.println("A");
+		else if (score>=80) System.out.println("B");
+		else if (score>=70) System.out.println("C");
+		else if (score>=60) System.out.println("D");
+		else System.out.println("F");
+		input.close();
+	}
+
+}
