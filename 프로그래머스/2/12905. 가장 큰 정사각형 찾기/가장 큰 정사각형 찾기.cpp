f06c83a37fe46326b8cf2b0bb4754904136e0cbc
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2024-09-28 08:13:44 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 6.33 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.cpp
@@ -4,20 +4,10 @@ using namespace std;
 
 int solution(vector<vector<int>> board)
 {
-    int answer = 0;
-    int n = board.size();
-    int m = board[0].size();
-    
-    vector<vector<int>> dp(n+1, vector<int>(m+1,0));
-    
-    for (int i=1; i<=n; i++) {
-        for (int j=1; j<=m; j++) {
-            if (board[i-1][j-1]) {
-                dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
-                answer = max(answer, dp[i][j]);
-            }
-        }
-    }
-    
-    return answer * answer;
+    int answer = 1234;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
 }
