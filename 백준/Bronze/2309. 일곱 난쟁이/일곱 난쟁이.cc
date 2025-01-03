997ab4fcdc633577404e27f95ef53da4f72e4b35
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-04 00:34:51 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int p[9];
+    int sum = -100;
+    int flag = 0;
+    for (int i=0; i<9; i++){
+        cin >> p[i];
+        sum += p[i];
+    }
+    sort(p, p+9);
+    for (int i=0; i<9; i++) {
+        for (int j=i+1; j<9; j++) {
+            if (flag == 0 && (p[i]+p[j]) == sum) {
+                p[i] = 0;
+                p[j] = 0;
+                flag = 1;
+                break;
+            }
+        }
+        if (p[i] != 0) {
+            cout << p[i] << "\n";
+        }
+    }
+}
