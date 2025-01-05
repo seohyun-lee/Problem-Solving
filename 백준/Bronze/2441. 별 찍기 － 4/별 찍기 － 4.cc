d8003cd1b69c0266f48e26c796cd935a80bf7ef8
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-05 23:27:25 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        for (int j=0; j<i; j++)
+            cout << " ";
+        for (int j=n-i-1; j>=0; j--)
+            cout << "*";
+        cout << "\n";
+    }
+
+    return 0;
+}
