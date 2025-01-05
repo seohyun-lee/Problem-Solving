0f7a2097cf7a6da95c7e4c7b510f648cb317f8a7
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-05 23:40:01 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        for (int j=0; j<=i; j++)
+            cout << "*";
+        for (int j=2*(n-i-1); j>0; j--)
+            cout << " ";
+        for (int j=0; j<=i; j++)
+            cout << "*";
+        cout << "\n";
+    }
+    for(int i=1; i<n; i++) {
+        for (int j=i; j<n; j++)
+            cout << "*";
+        for (int j=0; j<2*i; j++)
+            cout << " ";
+        for (int j=i; j<n; j++)
+            cout << "*";
+        cout << "\n";
+    }
+
+    return 0;
+}
