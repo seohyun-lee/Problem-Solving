f0ed81c17d4ac021dd419acb5e5f39d3b932ce5c
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-14 15:50:11 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,60 +1,38 @@
 #include <iostream>
-#include <vector>
 #include <queue>
-
 using namespace std;
-const int SIZE = 1e4 + 1; //1: 실제로 원소가 저장되지 않는 범위 인덱스. 0번 인덱스.
 
-vector<int> queue_vec(SIZE);
-int front_pointer = 0, rear_pointer = 0;
-
-int main() {
-	int n, x;
-	string cmd;
-	queue<int> q;
-	cin >> n;
-	while (n--) {
-		cin >> cmd;
-		if (cmd == "push") {
-			cin >> x;
-			q.push(x);
-			continue;
-		}
-		if (cmd == "pop") {
-			if (q.empty()) {
-				cout << "-1\n";
-			}
-			else {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-			continue;
-		}
-		if (cmd == "size") {
-			cout << q.size() << '\n';
-			continue;
-		}
-		if (cmd == "empty") {
-			cout << q.empty() << '\n';
-			continue;
-		}
-		if (cmd == "front") {
-			if (q.empty()) {
-				cout << "-1\n";
-			}
-			else {
-				cout << q.front() << '\n';
-			}
-			continue;
-		}
-		if (cmd == "back") {
-			if (q.empty()) {
-				cout << "-1\n";
-			}
-			else {
-				cout << q.back() << '\n';
-			}
-		}
-	}
-	return 0;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    queue<int> Q;
+    for (int i=0; i<n; i++) {
+        string s;
+        cin >> s;
+        if (s == "push") {
+            int x;
+            cin >> x;
+            Q.push(x);
+        } else if (s == "pop") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else {
+                cout << Q.front() << "\n";
+                Q.pop();
+            }
+        } else if (s == "size") {
+            cout << Q.size() << "\n";
+        } else if (s == "front") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else cout << Q.front() << "\n";
+        } else if (s == "back") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else cout << Q.back() << "\n";
+        } else if (s == "empty") {
+            cout << Q.empty() << "\n";
+        }
+    }
 }
