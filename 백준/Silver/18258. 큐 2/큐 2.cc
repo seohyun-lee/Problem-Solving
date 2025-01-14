22ce81b11d4992944ccdec6141cb39774d7fc9a0
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-14 15:57:57 +0900

    [Silver IV] Title: 큐 2, Time: 324 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    queue<int> Q;
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        string s;
+        cin >> s;
+        if (s == "push") {
+            int x;
+            cin >> x;
+            Q.push(x);
+        } else if (s == "pop") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else {
+                cout << Q.front() << "\n";
+                Q.pop();
+            }
+        } else if (s == "size") {
+            cout << Q.size() << "\n";
+        } else if (s == "front") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else cout << Q.front() << "\n";
+        } else if (s == "back") {
+            if (Q.empty()) cout << "-1" << "\n";
+            else cout << Q.back() << "\n";
+        } else if (s == "empty") {
+            cout << Q.empty() << "\n";
+        }
+    }
+}
