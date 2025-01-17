fd7e950d46499ac8670fa67fc7c4a844767b0aa5
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-01-17 22:59:48 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,43 +1,35 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <map>
-
 using namespace std;
 
-bool isBalanced(string &s) {
-	stack<char> st;
-	map<char, char> m;
-	m[')'] = '(';
-	m[']'] = '[';
-	for (int i = 0; i < s.length(); i++) {
-		switch (s[i]) {
-		case '(': case '[':
-			st.push(s[i]);
-			break;
-		case ')': case ']':
-			if (st.empty() || st.top() != m[s[i]]) { //비어있거나 괄호 종류 안맞음
-				return false;
-			}
-			st.pop(); //괄호 종류 맞음
-			break;
-		}
-	}
-	return st.empty();
-}
-int main() {
-	string s;
-	//getline(cin, <문자열 이름>): 공백 포함 입력받기
-	while (getline(cin, s)) {
-		if (s == ".") {
-			break;
-		}
-		if (isBalanced(s)) {
-			cout << "yes\n";
-		}
-		else {
-			cout << "no\n";
-		}
-	}
-	return 0;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    char arr[4] = {'(', ')', '[', ']'};
+    string s;
+    getline(cin, s);
+    while (s != ".") {
+        stack<int> stk;
+        bool flag=true;
+        for (char c : s) {
+            if (c == arr[0] || c == arr[2])
+                stk.push(c);
+            else if (c == arr[1] || c == arr[3]) {
+                int matchidx = (c == arr[1]) ? 0 : 2;
+                if (!stk.empty() && stk.top() == arr[matchidx]) {
+                    stk.pop();
+                }
+                else {
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        if (flag && stk.empty()) cout << "yes\n";
+        else cout << "no\n";
+        getline(cin, s);
+    }
 }
