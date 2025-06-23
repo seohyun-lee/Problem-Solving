32b0b6e007ca864ddbbd52ccbe512a7be0a87b85
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-06-23 16:01:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.17 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,29 @@
+class Solution {
+    private final String[] WORDS = {"aya", "ye", "woo", "ma"};
+    
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String s : babbling) {
+            if(isValid(s, new boolean[WORDS.length]))
+                answer++;
+        }
+        return answer;
+    }
+    
+    private boolean isValid(String s, boolean[] used) {
+        if(s.isEmpty())
+            return true;
+        for(int i=0; i<used.length; i++){
+            if(!used[i] && s.startsWith(WORDS[i])) {
+                // 중간 매치 단어를 지울 경우 단어를 빼고 나서 합쳐지는 걸 고려해야 하기 때문에
+                // startsWith로 앞에 있는지를 확인하는 것이 편리
+                used[i] = true;
+                if(isValid(s.substring(WORDS[i].length()), used)) {
+                    return true;
+                }
+                used[i] = false;
+            }
+        }
+        return false;
+    }
+}
