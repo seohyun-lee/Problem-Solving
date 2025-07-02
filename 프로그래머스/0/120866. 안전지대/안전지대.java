32547d59bd4969cdb2d9c48aa48284466ca780d2
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-03 00:48:06 +0900

    [level 0] Title: 안전지대, Time: 0.53 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -4,18 +4,22 @@ class Solution {
         int[] dx = {-1, 0, 1};
         int[] dy = {-1, 0, 1};
         int n = board[0].length;
-        boolean[][] mineArea = new boolean[n+2][n+2];
         for (int i=0; i<n*n; i++) {
             if (board[i/n][i%n]==1) {
                 for (int j=0; j<3; j++) {
                     for (int k=0; k<3; k++) {
-                        mineArea[i/n+1+dx[j]][i%n+1+dy[k]]=true;
+                        try {
+                            if (board[i/n+dx[j]][i%n+dy[k]]==0)
+                                board[i/n+dx[j]][i%n+dy[k]]=2;
+                        } catch (Exception e) {
+                        }
                     }
                 }
             }
         }
         for (int i=0; i<n*n; i++) {
-            if (mineArea[i/n+1][i%n+1]==false) answer++;
+            if (board[i/n][i%n]==0)
+                answer++;
         }   
         return answer;
     }
