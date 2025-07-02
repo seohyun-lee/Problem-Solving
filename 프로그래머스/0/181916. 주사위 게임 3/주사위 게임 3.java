a8e6f50bde3ee2351848852d18486e5fe675aeb1
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-03 00:25:19 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,39 +1,6 @@
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        int[] arr = new int[7];
-        arr[a]++;
-        arr[b]++;
-        arr[c]++;
-        arr[d]++;
-        int max_count=0;
-        for (int i=1; i<7; i++) {
-            if (arr[i]==4) {
-                return 1111*i;
-            } else if (arr[i]==3) {
-                int j=1;
-                for (j=1; j<7; j++) {
-                    if(arr[j]==1) break;
-                }
-                return (10*i+j)*(10*i+j);
-            } else if (arr[i]==2) {
-                int q=0, r=0;
-                int j=1;
-                for (j=1; j<7; j++) {
-                    if(arr[j]!=0 && j!=i) {
-                        if (q!=0) r=j;
-                        else q=j;
-                    }
-                }
-                if (r==0) {
-                    return i>q ? (i+q)*(i-q) : (i+q)*(q-i);
-                } else {
-                    return (q*r);
-                }
-            }
-        }
-        for (int i=1; i<7; i++) {
-            if (arr[i]!=0) return i;
-        }
-        return 0;
+        int answer = 0;
+        return answer;
     }
 }
