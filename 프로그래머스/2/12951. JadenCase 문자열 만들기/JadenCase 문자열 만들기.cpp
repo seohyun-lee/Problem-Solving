2d21325e0878b0e6d908dd714a4c94ec0a481ba2
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-03 01:47:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    for (int i=0; i<s.length(); i++) {
+        if(s[i] >= 'a' && s[i] <= 'z' && (i == 0 || s[i-1] == ' ')) {
+            s[i] -= ('a' - 'A');
+        } else if (s[i] >= 'A' && s[i] <= 'Z' && !(i == 0 || s[i-1] == ' ')) {
+            s[i] += ('a' - 'A');
+        }
+    }
+    return s;
+}
