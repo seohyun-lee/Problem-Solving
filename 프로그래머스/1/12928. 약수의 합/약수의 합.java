e6f7aaa61d8a7e0f0caa46d7aab2acd50f63257d
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-03 00:52:51 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -3,9 +3,9 @@ class Solution {
         int answer = 0;
         for(int i=1; i*i<=n; i++){
             if (i*i==n) {
-                answer = answer + i;
+                answer += i;
             } else if (n%i==0) {
-                answer = answer + i + n/i;
+                answer += (i + n/i);
             }
         }
         return answer;
