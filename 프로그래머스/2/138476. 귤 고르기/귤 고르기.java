a9a82ae839ddde163f69f5e97763e5ab7ed55fef
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-04 03:32:25 +0900

    [level 2] Title: 귤 고르기, Time: 76.12 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -4,23 +4,16 @@ class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i : tangerine) {
-            if (map.containsKey(i)) {
-                map.put(i, map.get(i)+1);
-            } else {
-                map.put(i, 1);
-            }
+            map.put(i, map.getOrDefault(i, 0)+1);
         }
         List<Integer> keySet = new ArrayList<>(map.keySet());
         keySet.sort((o1, o2) -> map.get(o2).compareTo(map.get(o1)));
         int cnt = 0;
         for(int i : keySet) {
-            if (k > 0) {
-                if (map.get(i) <= k)
-                    k -= map.get(i);
-                else
-                    k = 0;
-                cnt++;
-            }
+            if (k <= 0)
+                break;
+            k -= map.get(i);
+            cnt++;
         }
         return cnt;
     }
