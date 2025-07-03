010ef3810039b9faef07277b8ff7ad36c9fdd898
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-04 04:34:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        List<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
         int index=0, turn=1;
         while (index < words.length) {
             for (int i=1; i<=n; i++) {
-                if ((list.contains(words[index])) || (index != 0 && (words[index-1].charAt(words[index-1].length()-1) != words[index].charAt(0)))) {
+                if ((set.contains(words[index])) || (index != 0 && (words[index-1].charAt(words[index-1].length()-1) != words[index].charAt(0)))) {
                     return new int[]{i, turn};
                 }
-                list.add(words[index++]);
+                set.add(words[index++]);
             }
             turn++;
         }
