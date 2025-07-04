9b7c9a5df52f269f7b6188ea4be824b9aaccddeb
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-04 18:07:37 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,14 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        for (int i=1; ; i++) {
-            if ((a == b-1 || a-1 == b) && (a+1)/2==(b+1)/2) {
-                return i;
-            }
-            n /= 2;
-            a = (a+1)/2;
-            b = (b+1)/2;
-        }
+        return Integer.toBinaryString((a-1)^(b-1)).length();
+        // for (int i=1; ; i++) {
+        //     if ((a == b-1 || a-1 == b) && (a+1)/2==(b+1)/2) {
+        //         return i;
+        //     }
+        //     n /= 2;
+        //     a = (a+1)/2;
+        //     b = (b+1)/2;
+        // }
     }
 }
