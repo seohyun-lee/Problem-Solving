e2c6838afca32021b95932d69ca21695f7c9e873
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-04 19:36:45 +0900

    Update 행렬의 곱셈.cpp

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -5,14 +5,11 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
     vector<vector<int>> answer(arr1.size(), vector<int>(arr2[0].size(), 0));
-    
     for (int i=0; i<arr1.size(); i++) {
         for (int j=0; j<arr2[0].size(); j++) {
-            int sum = 0;
             for (int k=0; k<arr1[0].size(); k++) {
-                sum += arr1[i][k] * arr2[k][j];
+                answer[i][j] += arr1[i][k] * arr2[k][j];
             }
-            answer[i][j] = sum;
         }
     }
     return answer;
