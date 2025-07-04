34ddefc41f5e188f3203eae726cbc8b7b850e247
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-04 18:55:19 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 105.56 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,22 +3,32 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] elements) {
-        int len = elements.length;
-        int[] extended = new int[len*2];
         Set<Integer> set = new HashSet<>();
+        int[] dp = new int[elements.length]; // 캐시로 사용
         
-        for(int i=0; i<len*2; i++) {
-            extended[i] = elements[i%len];
-        }
-        for(int i=0; i<len; i++) { // 갯수
-            for (int j=0; j<len; j++) { // 시작점
-                int sum = 0;
-                for (int k=j; k<=j+i; k++) {
-                    sum += extended[k];
-                }
-                set.add(sum);
+        for(int len=1; len <= elements.length; len++) {
+            for(int i=0; i<elements.length; i++) {
+                dp[i] += elements[(i+len-1) % elements.length];
+                set.add(dp[i]);
+                // dp[i]: 인덱스 i에서 시작하는 길이 len짜리 연속 부분 수열의 합
+                // 이전 dp[i] (len-1 길이 합)에 elements[(i+len-1) % elements.length]를 더해 갱신
+
             }
         }
         return set.size();
+        
+//         int len = elements.length;
+//         Set<Integer> set = new HashSet<>();
+        
+//         for(int i=0; i<len; i++) { // 갯수
+//             for (int j=0; j<len; j++) { // 시작점
+//                 int sum = 0;
+//                 for (int k=j; k<=j+i; k++) {
+//                     sum += elements[k%len];
+//                 }
+//                 set.add(sum);
+//             }
+//         }
+//         return set.size();
     }
 }
