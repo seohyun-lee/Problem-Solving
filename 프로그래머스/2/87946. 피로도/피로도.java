5551941e5a27f7a880a0cdf841a113698e0021b2
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-05 02:47:22 +0900

    [level 2] Title: 피로도, Time: 3.60 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,21 @@
+class Solution {
+    int max = 0;
+    
+    private void goDungeon(int currentFatigue, boolean[] visited, int[][] dungeons, int cnt){
+        max = Math.max(max, cnt);
+        
+        for(int i=0; i<dungeons.length; i++){
+            if(visited[i]) continue;
+            if (currentFatigue >= dungeons[i][0]) {
+                visited[i] = true;
+                goDungeon(currentFatigue-dungeons[i][1], visited, dungeons, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        goDungeon(k, visited, dungeons, 0);
+        return max;
+    }
+}
