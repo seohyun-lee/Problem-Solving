c3316480f39ba3da5c00394e6189bbe632a85909
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-05 02:54:35 +0900

    Update 피로도.java

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -2,17 +2,16 @@ class Solution {
     int max = 0;
     
     private void goDungeon(int currentFatigue, boolean[] visited, int[][] dungeons, int cnt){
-        max = Math.max(max, cnt);
-        
         for(int i=0; i<dungeons.length; i++){
-            if(visited[i]) continue;
-            if (currentFatigue >= dungeons[i][0]) {
+            if(!visited[i] && currentFatigue >= dungeons[i][0]) {
                 visited[i] = true;
                 goDungeon(currentFatigue-dungeons[i][1], visited, dungeons, cnt+1);
                 visited[i] = false;
             }
         }
+        max = Math.max(max, cnt);
     }
+    
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
         goDungeon(k, visited, dungeons, 0);
