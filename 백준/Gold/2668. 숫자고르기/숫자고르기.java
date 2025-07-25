1ced7b7fa911cbe4406b0882b6617653cd28a3df
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-07-26 06:24:21 +0900

    [Gold V] Title: 숫자고르기, Time: 96 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -2,37 +2,50 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main
-{
+public class Main {
+    static int[] arr;
+    static boolean[] visited;
+    static boolean[] finished;
+    static Set<Integer> result = new TreeSet<>(); // 자동 정렬 + 중복 제거
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n];
+        visited = new boolean[n];
+        finished = new boolean[n];
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine())-1;
+            arr[i] = Integer.parseInt(br.readLine()) - 1;
         }
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            int start = i;
-            int end = arr[start];
-            List<Integer> candList = new ArrayList<>();
-            for(int j=0; j<n; j++) {
-                if (start == end) {
-                    candList.add(end);
-                    for(Integer in : candList) {
-                        if (!list.contains(in+1))
-                            list.add(in+1);
-                    }
-                    break;
-                } else {
-                    candList.add(start);
-                    end = arr[end];
-                }
-            }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) dfs(i);
         }
-        System.out.println(list.size());
-        for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
+        System.out.println(result.size());
+        for (int num : result) {
+            System.out.println(num + 1); // index 복원
+        }
+    }
+
+    static void dfs(int curr) {
+        visited[curr] = true;
+        int next = arr[curr];
+
+        if (!visited[next]) {
+            dfs(next);
+        } else if (!finished[next]) {
+            extractCycle(next, curr);
+        }
+        finished[curr] = true;
+    }
+
+    static void extractCycle(int start, int end) {
+        int node = start;
+        result.add(node);
+        while (node != end) {
+            node = arr[node];
+            result.add(node);
         }
     }
 }
