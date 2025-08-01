9a4cdb5ccd57b2dbb11c0fed99b8fa78f17a59cc
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-08-01 15:50:37 +0900

    [Gold V] Title: 배, Time: 168 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -3,39 +3,47 @@ import java.util.*;
 
 public class Main
 {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] crane = new int[n];
+        Integer[] crane = new Integer[n];
         String[] sb = br.readLine().split(" ");
         for(int i=0; i<n; i++) {
             crane[i] = Integer.parseInt(sb[i]);
         }
-        Arrays.sort(crane); // 오름차순 정렬
+        Arrays.sort(crane, Collections.reverseOrder());
+
         int m = Integer.parseInt(br.readLine());
-        List<Integer> box = new ArrayList<>();
+        Integer[] box = new Integer[m];
         sb = br.readLine().split(" ");
         for(int i=0; i<m; i++) {
-            box.add(Integer.parseInt(sb[i]));
-            if (box.get(i) > crane[n-1]) { // 모든 박스를 배로 옮길 수 없으면 -1 출력
+            box[i] = Integer.parseInt(sb[i]);
+            if (box[i] > crane[0]) {
                 System.out.println(-1);
                 return;
             }
         }
-        Collections.sort(box); // 오름차순 정렬
-        
+        Arrays.sort(box, Collections.reverseOrder());
+
+        boolean[] used = new boolean[m];
+        int[] pos = new int[n];
+        int moved = 0;
         int time = 0;
-        while (box.size() > 0) {
-            for(int i=n-1; i>=0; i--) {
-                for(int j=box.size()-1; j>=0; j--) {
-                    if (crane[i] >= box.get(j)) {
-                        box.remove(j);
+
+        while (moved < m) {
+            for (int i = 0; i < n; i++) {
+                while (pos[i] < m) {
+                    if (!used[pos[i]] && crane[i] >= box[pos[i]]) {
+                        used[pos[i]] = true;
+                        moved++;
+                        pos[i]++;
                         break;
                     }
+                    pos[i]++;
                 }
             }
             time++;
         }
         System.out.println(time);
-	}
+    }
 }
