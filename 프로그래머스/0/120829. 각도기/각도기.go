e5f3facd5426926f9edfa37d747a5c0f6c86a5a9
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-08-21 20:02:04 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.go
@@ -0,0 +1,11 @@
+func solution(angle int) int {
+    if angle < 90 {
+        return 1
+    } else if angle == 90 {
+        return 2
+    } else if angle < 180 {
+        return 3
+    } else {
+        return 4
+    }
+}
