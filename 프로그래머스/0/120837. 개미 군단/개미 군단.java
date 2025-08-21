a6894b805d034345fe9b73e7971b74179e01fbe5
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-08-21 21:39:56 +0900

    [level 0] Title: 개미 군단

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        while(hp/5>0){
-            answer+=(hp/5);
-            hp%=5;
-        }
-        while(hp/3>0){
-            answer+=(hp/3);
-            hp%=3;
-        }
-        while(hp>0){
-            answer++;
-            hp--;
-        }
-        return answer;
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
     }
 }
