bf50fb5e6641ce007cb0c779dd5dfc0e020568d4
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-08-22 02:04:34 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.03 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,7 +1,6 @@
 class Solution {
     public static int gcd(int a, int b){
         if(a%b==0) return b;
-        System.out.println(a+" "+b);
         return gcd(b, a%b);
     }
     public int[] solution(int n, int m) {
