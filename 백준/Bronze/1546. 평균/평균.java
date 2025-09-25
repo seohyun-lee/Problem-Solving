7ced9dd429f382992f347e3c2200b74c3ee61d43
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-09-25 16:42:35 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n, m=0;
-		n=sc.nextInt();
-		int[] score = new int[1000];
-		double average=0;
-		for(int i=0;i<n;i++) {
-			score[i]=sc.nextInt();
-			if(score[i]>m)
-				m=score[i];
-			average+=score[i];
-		}
-		average=average/m*100/n;
-		System.out.println(average);
+public class Main
+{
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int n = Integer.parseInt(br.readLine());
+	    String[] s = br.readLine().split(" ");
+	    long sum=0, max=0;
+	    for(int i=0; i<n; i++) {
+	        int score = Integer.valueOf(s[i]);
+			if(max < score)
+				max = score;
+	        sum += score;
+	    }
+	    System.out.println(sum*100.0/max/n);
 	}
-
 }
