126ae3ad39b2b8778485c07a8143902310764f39
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2025-09-27 18:31:37 +0900

    [Silver I] Title: 미로 탐색, Time: 120 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class Main
 {
     static int n, m, target;
-    static int[][] map;
-    static List<Integer>[] A;
-    static boolean[] visited;
-    static int[] walk;
+    static int[][] A;
+    static boolean[][] visited;
+    static int[] dx={0, 1, 0, -1};
+    static int[] dy={1, 0, -1, 0};
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,55 +15,34 @@ public class Main
         n = Integer.parseInt(st.nextToken()); // 정점 수
         m = Integer.parseInt(st.nextToken()); // 간선 수
         
-        map = new int[n][m];
-        A = new ArrayList[n*m];
-        visited = new boolean[n*m];
+        A = new int[n][m];
+        visited = new boolean[n][m];
         for(int i=0; i<n; i++){
             String s = br.readLine();
             for(int j=0; j<m; j++){ 
-                map[i][j] = s.charAt(j)-'0';
-                A[i*m+j] = new ArrayList<>();
+                A[i][j] = s.charAt(j)-'0';
             }
         }
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){ 
-                if(map[i][j]==1){
-                    if(j<m-1 && map[i][j+1]==1)
-                        A[i*m+j].add(i*m+(j+1));
-                    if (i<n-1 && map[i+1][j]==1)
-                        A[i*m+j].add((i+1)*m+j);
-                    if (j>0 && map[i][j-1]==1)
-                        A[i*m+j].add(i*m+(j-1));
-                    if (i>0 && map[i-1][j]==1)
-                        A[i*m+j].add((i-1)*m+j);
-                }
-            }
-        }
-        
-        walk = new int[n*m];
-        
-        // 도착 지점 좌표
-        target = n*m-1;
-        bfs(0);
-        System.out.println(walk[target]);
+        bfs(0, 0);
+        System.out.println(A[n-1][m-1]);
 	}
-	static void bfs(int start){
-	    Queue<Integer> q = new LinkedList<>();
-	    visited[start]=true;
-	    q.add(start);
-	    walk[start]=1;
+	
+	static void bfs(int i, int j){
+	    Queue<int[]> q = new LinkedList<>();
+	    q.offer(new int[]{i, j});
+	    visited[i][j]=true;
 	    
 	    while(!q.isEmpty()){
-	        int now = q.poll();
-	        if(now==target){
-	            break;
-	        }
-	        for(int i : A[now]){
-	            if(!visited[i]){
-	                visited[i] = true;
-	                q.add(i);
-	                walk[i]=walk[now]+1;
+	        int[] now = q.poll();
+	        for(int k=0; k<4; k++){
+	            int x= now[0]+dx[k];
+	            int y= now[1]+dy[k];
+	            if(x>=0&&y>=0&&x<n&&y<m){
+	                if(A[x][y]!=0&&!visited[x][y]){
+	                    visited[x][y]=true;
+	                    A[x][y]=A[now[0]][now[1]]+1; // 깊이 업데이트
+	                    q.add(new int[]{x, y});
+	                }
 	            }
 	        }
 	    }
