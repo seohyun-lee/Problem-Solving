cdf3e6f90c31aeebd3fb920602aa817f13c68f9a
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2026-03-09 00:25:50 +0900

    [Gold IV] Title: 타일 채우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,12 @@
+n = int(input())
+if n % 2 == 1:
+    print(0)
+else:
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[2] = 3
+    for i in range(4, n + 1, 2):
+        dp[i] = dp[i-2] * 3
+        for j in range(i-4, -1, -2):
+            dp[i] += dp[j] * 2
+    print(dp[n])
