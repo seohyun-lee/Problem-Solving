569433f2285b061c960f627885b277269cf8ba21
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2026-04-10 20:38:47 +0900

    [level 4] Title: 사칙연산, Time: 2.80 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -4,42 +4,30 @@
 
 using namespace std;
 
+int calc(vector<string> arr, int start, int end, bool bigger){
+    if(start==end)
+        return stoi(arr[start]);
+
+    int sum;
+    if (bigger) sum = -2100000000;
+    else sum = 2100000000;
+    
+    for(int m=start+1; m<end; m+=2){
+        int res=0;
+        if(arr[m]=="+"){
+           res = calc(arr, start, m-1, true) + calc(arr, m+1, end, true);
+        } else{
+           res = calc(arr, start, m-1, true) - calc(arr, m+1, end, false);
+        }
+        if (bigger) sum = max(sum, res);
+        else sum = min(sum, res);
+    }
+    return sum;
+}
+
 int solution(vector<string> arr)
 {
-    int max_dp[102][102] = {0,};
-    int min_dp[102][102] = {0,};
-    int numSize = arr.size()/2+1;
-    
-    // 자기자신 넣고 + 초기화
-    for(int i=0; i<numSize; i++){
-        for(int j=0; j<numSize; j++) {
-            if(i==j){
-                max_dp[i][j] = stoi(arr[i*2]);
-                min_dp[i][j] = stoi(arr[i*2]);
-            } else {
-                max_dp[i][j] = -1000000000;
-                min_dp[i][j] = 1000000000;
-            }
-        }
-    }
-    
-    // DP 최댓값과 최소값을 입력해나가며 계산
-    for(int idx=1; idx<numSize; idx++){
-        for(int i=0; i<numSize-idx; i++) {
-            int j=idx+i;
-            
-            for(int k=i; k<j; k++){
-                if(arr[k*2+1] == "-") {
-                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] - min_dp[k+1][j]);
-                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] - max_dp[k+1][j]);
-                }
-                else if(arr[k * 2 + 1] == "+")
-                {
-                    max_dp[i][j] = max(max_dp[i][j], max_dp[i][k] + max_dp[k+1][j]);
-                    min_dp[i][j] = min(min_dp[i][j], min_dp[i][k] + min_dp[k+1][j]);
-                }
-            }
-        }
-    }
-    return max_dp[0][numSize-1];
+    int answer = -1;
+    int len = arr.size();
+    return calc(arr, 0, len-1, true);
 }
