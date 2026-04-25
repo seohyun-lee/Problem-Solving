9f3b93039a07a2319675a62802af0a5efdb590f0
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2026-04-25 16:06:37 +0900

    [Gold V] Title: 강의실, Time: 632 ms, Memory: 57008 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.java
+++ 백준/Gold/1374. 강의실/강의실.java
@@ -3,27 +3,20 @@ import java.util.*;
 
 public class Main {
     static int solution(int N, int[][] lectures) {
-        int cnt=1;
-        int[] prevEnds = new int[N];
-        Arrays.sort(lectures, (a, b)->{
-            if(a[1]==b[1]) return Integer.compare(a[2], b[2]);
+        // 시작 시간 기준 정렬
+        Arrays.sort(lectures, (a, b) -> {
+            if (a[1] == b[1]) return Integer.compare(a[2], b[2]);
             return Integer.compare(a[1], b[1]);
         });
-        prevEnds[0]=lectures[0][2];
-        for(int i=1; i<N; i++) {
-            int j=0;
-            for(j=0; j<cnt; j++) {
-                if(prevEnds[j]<=lectures[i][1]){ // 새 강의 시작 전에 이전 것 끝나야
-                    prevEnds[j]=lectures[i][2];
-                    break;
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 최소 힙
+        pq.offer(lectures[0][2]);
+        for (int i = 1; i < N; i++) {
+            if (pq.peek() <= lectures[i][1]) {
+                pq.poll(); // 가장 빨리 끝나는 것을 제거할 수 있는지
             }
-            if(j==cnt){
-                cnt++;
-                prevEnds[cnt-1]=lectures[i][2];
-            }
+            pq.offer(lectures[i][2]);
         }
-        return cnt;
+        return pq.size();
     }
 
     public static void main(String[] args) throws Exception {
