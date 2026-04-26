0683d70322563560c1b06843a9182d542623fd24
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2026-04-26 12:03:37 +0900

    [level 4] Title: 도둑질, Time: 19.12 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int rob(int[] money, int start, int end){
+        int[] dp = new int[money.length];
+        dp[start]=money[start];
+        dp[start+1]=Math.max(dp[start], money[start+1]);
+        for(int i=start+2; i<=end; i++){
+            if(dp[i-2]+money[i]>dp[i-1]){ // 지금 것 선택
+                dp[i]=dp[i-2]+money[i];
+            } else { // 지금 것 미선택
+                dp[i]=dp[i-1];
+            }
+        }
+        return dp[end];
+    }
+    public int solution(int[] money) {
+        int answer = 0;
+        int n = money.length;
+        return Math.max(rob(money, 0, n-2), rob(money, 1, n-1));
+    }
+}
